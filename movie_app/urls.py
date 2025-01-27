148355123d7c99aa945b603102ec39ce282e6405
Bishkek25 <kunduz.qw8@gmail.com>
2025-01-27 17:06:23 +0600

    fix bugs

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('directors/', views.DirectorDetailAPIView.as_view(), name='directors-list'),
+    path('directors/', views.DirectorListAPIView.as_view(), name='directors-list'),
     path('directors/<int:id>/', views.DirectorDetailAPIView.as_view(), name='directors-detail'),
     path('movies/', views.MovieListAPIView.as_view(), name='movies-list'),
     path('movies/<int:id>/', views.MovieDetailAPIView.as_view(), name='movies-detail'),
